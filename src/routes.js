c3e784bc1106b9e67e7bafb04e88ac7577a9dc8e
Willy Nugraha <willymiiee@gmail.com>
2018-08-20 14:46:18 +0800

    add reservation & reservationDetail on routes

--- src/routes.js
+++ src/routes.js
@@ -20,12 +20,24 @@ const UserDetail = Loadable({
   loading: Loading,
 })
 
+const Reservation = Loadable({
+  loader: () => import('./components/Reservations'),
+  loading: Loading,
+})
+
+const ReservationDetail = Loadable({
+  loader: () => import('./components/ReservationDetail'),
+  loading: Loading,
+})
+
 // https://github.com/ReactTraining/react-router/tree/master/packages/react-router-config
 const routes = [
   { path: '/dashboard', name: 'Dashboard', component: Dashboard },
   { path: '/users/:type/add', name: 'Add User', component: UserDetail },
   { path: '/users/:type/:id', name: 'User Detail', component: UserDetail },
   { path: '/users/:type', name: 'User', component: User },
+  { path: '/reservations/:id', name: 'Reservation Detail', component: ReservationDetail },
+  { path: '/reservations', name: 'Reservation', component: Reservation },
 ]
 
 export default routes
