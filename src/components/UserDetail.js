0b5123141b188223bee7f4ce8423741e500e9ebe
Ramaditia D <rdwiyan.saputra@gmail.com>
2018-10-23 17:55:26 +0800

    fix undefined path parameter

--- src/components/UserDetail.js
+++ src/components/UserDetail.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
+import { withRouter } from 'react-router-dom';
 import {
   Card,
   CardHeader,
@@ -764,4 +765,4 @@ class UserDetail extends Component {
 
 export default connect(null, {
   fetchCountries
-})(UserDetail);
+})(withRouter(UserDetail));
